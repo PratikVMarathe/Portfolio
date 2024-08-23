36f3fc050a5deeae36e3d36b20de717250759e2b
Pratik Marathe <86453383+PratikVMarathe@users.noreply.github.com>
2024-08-23 22:22:37 +0530

    Update main.js
    Added animation Scroll section

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,6 +145,18 @@ function scrollActive() {
 }
 window.addEventListener("scroll", scrollActive);
 
+// SCROLL SECTIONS
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    entry.isIntersecting
+      ? entry.target.classList.add("show")
+      : entry.target.classList.remove("show");
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el)=>observer.observe(el));
+
 // HEADER SHADOW
 function scrollHeader() {
   const nav = document.getElementById("header");
